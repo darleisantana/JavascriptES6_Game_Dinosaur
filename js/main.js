29b5dbdcdba0d2cd6e768015d471bc25c21697a1
Darleysantana <darleyhyuga@gmail.com>
2021-05-25 16:28:06 -0300

    Adicionado movimentação!

--- js/main.js
+++ js/main.js
@@ -3,6 +3,31 @@ console.log(dino);
 function handleKeyUp(event){
     if(Event.keyCode === 32){
         console.log('Pressionou Espaço!');
+        jump();
     }
+}
+function jump(){
+    let position = 0;
+    let upInterval = setInterval(() =>{
+      if(position >= 150)
+        {
+            clearInterval(upInterval);
+            //Descendo
+            let downInterval = setInterval(() =>{
+                if(position <= 0){
+                    clearInterval(downInterval);
+                }else{
+                    position -= 20;
+                    dino.style.bottom = position + 'px';
+                }
+            }, 20);
+        }
+      else
+        {
+            //Subindo
+            position += 20;
+            dino.style.bottom = position + 'px';
+        }
+    }, 20);
 }
 document.addEventListener('keyup', handleKeyUp);
