ab67877e2228119da9ca04a44f8c7b418ae665db
Darleysantana <darleyhyuga@gmail.com>
2021-05-25 17:07:20 -0300

    Create Cactus!

--- js/main.js
+++ js/main.js
@@ -1,13 +1,20 @@
 const dino = document.querySelector('.dino');
+const background = document.querySelector('.background');
+let isJumping = false;
 console.log(dino);
+//Evento
 function handleKeyUp(event){
     if(Event.keyCode === 32){
         console.log('Pressionou Espaço!');
-        jump();
+        if(!isJumping){
+            jump();
+        }
     }
 }
+//movimentação
 function jump(){
     let position = 0;
+    isJumping = true;
     let upInterval = setInterval(() =>{
       if(position >= 150)
         {
@@ -16,6 +23,7 @@ function jump(){
             let downInterval = setInterval(() =>{
                 if(position <= 0){
                     clearInterval(downInterval);
+                    isJumping = false;
                 }else{
                     position -= 20;
                     dino.style.bottom = position + 'px';
@@ -30,4 +38,12 @@ function jump(){
         }
     }, 20);
 }
+function createCactus(){
+    const cactus = document.createElement('div');
+    let cactusposition = 1000;
+    cactus.classList.add('cactus');
+    cactus.style.left = 1000 + 'px';
+    background.appendChild(cactus);
+}
+createCactus();
 document.addEventListener('keyup', handleKeyUp);
