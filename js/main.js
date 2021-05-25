9d22bdc3d9a952acf8dc2512f8c6321c00d36177
Darleysantana <darleyhyuga@gmail.com>
2021-05-25 17:26:49 -0300

    Moving Cactus and Create Random Cactus!

--- js/main.js
+++ js/main.js
@@ -41,9 +41,23 @@ function jump(){
 function createCactus(){
     const cactus = document.createElement('div');
     let cactusposition = 1000;
+    let randomTime = Math.random() * 6000;
+
     cactus.classList.add('cactus');
     cactus.style.left = 1000 + 'px';
     background.appendChild(cactus);
+
+    let leftInterval = setInterval(() => {
+        
+        if(cactusposition <= -60){
+            clearInterval(leftInterval);
+            background.removeChild(cactus);
+        } else {
+            cactusposition -= 10;
+            cactus.style.left = cactusposition + 'px';
+        }
+    }, 20);
+    setTimeout(createCactus, randomTime);
 }
 createCactus();
 document.addEventListener('keyup', handleKeyUp);
